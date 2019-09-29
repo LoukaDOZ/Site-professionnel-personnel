5a96c867429969963a07b408e00e78cfded80e3b
LoukaDOZ <46566140+LoukaDOZ@users.noreply.github.com>
2019-09-29 13:51:28 +0200

    Update Home.php

--- views/Home.php
+++ views/Home.php
@@ -10,7 +10,7 @@
       <div class='contenu'>
         <h2>Bienvenue sur mon site</h2>
     		Je m'appelle Louka DOZ, je suis actuellement étudiant en seconde année de DUT Informatique à l'Université Paris-Est Créteil (UPEC) site de Fontainebleau.<br><br>
-    		<strong>Je cherche un stage dans le domaine de la programmation de jeux vidéo ou dans le developpement de sites web.</strong><br><br>
+    		<strong>Je cherche un stage dans le domaine de la programmation de jeux vidéo ou dans le développement de sites web.</strong><br><br>
     		Je souhaite découvrir les secrets de programmation des plus grands jeux vidéo et devenir moi même programmeur dans ce domaine. Pour autant, je reste tout autant intéressé par le web, la création et le design.<br>
     		Parcourez le site pour en découvrir plus sur moi.
       </div>
